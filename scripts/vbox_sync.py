f6d64d7c5b66fa2846502acac49b6e8b9b9d7f57
Serhii5465 <serhiiartiukh5@gmail.com>
2024-04-22 14:05:53 +0300

    vbox_sync.py - Update

--- scripts/vbox_sync.py
+++ scripts/vbox_sync.py
@@ -1,13 +1,24 @@
 import argparse
 import sys
 import os
-from typing import Dict
+import subprocess
+from typing import Dict, List
+
+def get_cmd_out(command: List[str]) -> subprocess.CompletedProcess:
+    return subprocess.run(command, capture_output=True, text=True)
+
+def run_cmd(command: List[str]) -> subprocess.CompletedProcess:
+    return subprocess.run(command, stderr=sys.stderr, stdout=sys.stdout)
+
+def get_list_vms() -> subprocess.CompletedProcess:
+    cmd = ['VBoxManage', 'list', 'vms']
 
 def parse_args() -> Dict[str, any]:
-    parser = argparse.ArgumentParser(description='Synchronization files between local storage and external HDD')
+    parser = argparse.ArgumentParser(description='Synchronization VMs with local/external storage')
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-d', '--download', action='store_true', help='Synchronization of VMs with external storage')
-    group.add_argument('-u', '--upload', action='store_true', help='Synchronization of VMs with internal storage')
+    group.add_argument('-d', '--download', action='store_true', help='Synchronization with external storage')
+    group.add_argument('-u', '--upload', action='store_true', help='Synchronization with internal storage')
+    group.add_argument('--list-vms', action='store_true', help='Lists all (VMs) that are currently registered with VirtualBox')
 
     args = vars(parser.parse_args())
 
@@ -19,9 +30,14 @@ def parse_args() -> Dict[str, any]:
 def main() -> None:
     os.environ["PATH"] += os.pathsep + os.pathsep.join(["/cygdrive/c/Program Files/Oracle/VirtualBox/"])
     # print(os.environ["PATH"])
+
     args = parse_args()
 
-
-
+    print(args)
+    if args['list_vms']:
+        get_list_vms()
+        # print(get_list_vms().stdout)
+        return
+    
 
 main()
